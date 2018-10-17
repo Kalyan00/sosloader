c5591f9a8d0153784254eeafd2299ce1c9a24a00
NKolomeitsev <npkolom1@mts.ru>
2018-10-17 21:30:12 +0300

    чуть адаптировал

--- sosloader/Program.cs
+++ sosloader/Program.cs
@@ -1,19 +1,16 @@
 ﻿using Microsoft.Diagnostics.Runtime;
-using Microsoft.Diagnostics.Runtime.Interop;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace sosloader
 {
     class DacLocator
     {
-        [DllImport("dbghelp.dll", SetLastError = true)]
+	    [DllImport("dbghelp.dll", SetLastError = true)]
         static extern bool SymInitialize(IntPtr hProcess, String symPath, bool fInvadeProcess);
 
         [DllImport("dbghelp.dll", SetLastError = true)]
@@ -50,112 +47,136 @@ namespace sosloader
             }
 
             Console.WriteLine("CLR version: " + clrInfo.Version);
-            string clrModuleName = clrInfo.Version.Major == 2 ? "mscorwks" : "clr";
-            StringBuilder loadedClrFile = new StringBuilder(2048);
-            if (!SymFindFileInPath(processHandle, null, clrModuleName + ".dll", clrInfo.DacInfo.TimeStamp,
-                clrInfo.DacInfo.FileSize, 0, 0x2, loadedClrFile, IntPtr.Zero, IntPtr.Zero))
-            {
-                Console.WriteLine("*** Error retrieving CLR from symbol server");
-            }
-            else
-            {
-                File.Copy(loadedClrFile.ToString(), Path.Combine(storageLocation, clrModuleName + ".dll"), true);
-            }
+            var clrModuleName = (clrInfo.Version.Major == 2 ? "mscorwks" : "clr")+ ".dll";
+	        var clrVersion = clrInfo.Version;
 
-            string str = (IntPtr.Size == 4) ? "x86" : "amd64";
-            VersionInfo clrVersion = clrInfo.Version;
-            string sosFileName = string.Format("sos_{0}_{1}_{2}.{3}.{4}.{5:D2}.dll",
-                str, target.Architecture, clrVersion.Major, clrVersion.Minor, clrVersion.Revision, clrVersion.Patch);
-            StringBuilder loadedSOSFile = new StringBuilder(2048);
-            if (!SymFindFileInPath(processHandle, null, sosFileName, clrInfo.DacInfo.TimeStamp,
-                clrInfo.DacInfo.FileSize, 0, 0x2, loadedSOSFile, IntPtr.Zero, IntPtr.Zero))
-            {
-                Console.WriteLine("*** Error retrieving SOS from symbol server");
-            }
-            else
-            {
-                File.Copy(loadedSOSFile.ToString(), Path.Combine(storageLocation, "SOS.dll"), true);
-            }
-
-            StringBuilder loadedDacFile = new StringBuilder(2048);
-            if (!SymFindFileInPath(processHandle, null, clrInfo.DacInfo.FileName, clrInfo.DacInfo.TimeStamp,
-                clrInfo.DacInfo.FileSize, 0, 0x2 /*SSRVOPT_DWORD*/, loadedDacFile, IntPtr.Zero, IntPtr.Zero))
-            {
-                Console.WriteLine("*** Error retrieving DAC from symbol server");
-            }
-            else
-            {
-                File.Copy(loadedDacFile.ToString(), Path.Combine(storageLocation, "mscordacwks.dll"), true);
-            }
+	        LoadSymbol(clrInfo, storageLocation, processHandle, clrModuleName, clrModuleName);
+			LoadSymbol(clrInfo, storageLocation, processHandle, $"sos_{(IntPtr.Size == 4 ? "x86" : "amd64")}_{target.Architecture}_{clrVersion.Major}.{clrVersion.Minor}.{clrVersion.Revision}.{clrVersion.Patch:D2}.dll", "SOS.dll");
+			LoadSymbol(clrInfo, storageLocation, processHandle, clrInfo.DacInfo.FileName, "mscordacwks.dll");
 
             SymCleanup(processHandle);
             return storageLocation;
         }
+
+	    private static void LoadSymbol(ClrInfo clrInfo, string storageLocation, IntPtr processHandle, string filename,
+		    string filename2)
+	    {
+
+		    for (int i = 0; i < 100; i++)
+		    {
+			    var loadedClrFile = new StringBuilder(2048);
+			    if (!SymFindFileInPath(processHandle, null, filename, clrInfo.DacInfo.TimeStamp,
+				    clrInfo.DacInfo.FileSize, 0, 0x2, loadedClrFile, IntPtr.Zero, IntPtr.Zero))
+			    {
+				    Console.WriteLine(
+					    $"*** Error retrieving {filename2} from symbol server. ErrorCode={Marshal.GetLastWin32Error()}");
+				    Thread.Sleep(10);
+			    }
+			    else
+			    {
+				    var destFileName = Path.Combine(storageLocation, filename2);
+					Console.WriteLine(destFileName);
+				    File.Copy(loadedClrFile.ToString(), destFileName, true);
+				    break;
+			    }
+		    }
+	    }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            if (args.Length != 2 || (args[0] != "download" && args[0] != "launch"))
-            {
-                Console.WriteLine("Usage: sosloader <download | launch> <dump file path>");
-                return;
-            }
+	        try
+	        {
 
-            Console.WriteLine("\nPlease make sure that you have dbghelp.dll and symsrv.dll accessible\nto the application. Otherwise, we will not be able to retrieve files\nfrom the Microsoft symbol server.\n");
+				if (args.Length == 1 && args[0].EndsWith(".dmp", StringComparison.InvariantCultureIgnoreCase))
+				{
+					args = new[] { "launch", args[0] };
+				}
 
-            string dumpFilePath = args[1];
-            DataTarget target = DataTarget.LoadCrashDump(dumpFilePath);
-            if (target.ClrVersions.Count == 0)
-            {
-                Console.WriteLine("This dump file does not have a CLR loaded in it.");
-                return;
-            }
-            if (target.ClrVersions.Count > 1)
-            {
-                Console.WriteLine("This dump file has multiple CLR versions loaded in it.");
-                return;
-            }
+				if (args.Length != 2 || (args[0] != "download" && args[0] != "launch"))
+				{
+					Console.WriteLine("Usage: sosloader <download | launch> <dump file path>");
+					return;
+				}
 
-            if (target.Architecture == Architecture.X86 && IntPtr.Size != 4)
-            {
-                Console.WriteLine("Please use the 32 bit version of sosloader to analyze this dump.");
-                return;
-            }
-            if (target.Architecture == Architecture.Amd64 && IntPtr.Size != 8)
-            {
-                Console.WriteLine("Please use the 64 bit version of sosloader to analyze this dump.");
-                return;
-            }
+				Console.WriteLine("\nPlease make sure that you have dbghelp.dll and symsrv.dll accessible\nto the application. Otherwise, we will not be able to retrieve files\nfrom the Microsoft symbol server.\n");
 
-            string dacLocation = target.ClrVersions[0].LocalMatchingDac;
-            if (!String.IsNullOrEmpty(dacLocation))
-            {
-                //No symbol load needed, the files are available on the local machine
-                Console.WriteLine("The debug support files are available on the local machine.");
-                if (args[0] == "launch")
-                {
-                    Console.WriteLine("Launching windbg.exe with the provided dump file...");
-                    string loadByCommand = ".loadby sos " + (target.ClrVersions[0].Version.Major == 4 ? "clr" : "mscorwks");
-                    Process.Start("windbg.exe", String.Format("-z {0} -c \"{1}\"", dumpFilePath, loadByCommand));
-                }
-                return;
-            }
+				string dumpFilePath = args[1];
+				DataTarget target = DataTarget.LoadCrashDump(dumpFilePath);
+				if (target.ClrVersions.Count == 0)
+				{
+					Console.WriteLine("This dump file does not have a CLR loaded in it.");
+					return;
+				}
+				if (target.ClrVersions.Count > 1)
+				{
+					Console.WriteLine("This dump file has multiple CLR versions loaded in it.");
+					return;
+				}
 
-            string debugSupportFilesLocation = DacLocator.GetDebugSupportFiles(target.ClrVersions[0], target);
-            if (args[0] == "launch")
-            {
-                Console.WriteLine("Launching windbg.exe with the provided dump file (must be in path)...");
-                string loadCommand = String.Format(".load {0}; .cordll -se -lp {1}",
-                    Path.Combine(debugSupportFilesLocation, "sos"), debugSupportFilesLocation);
-                Process.Start("windbg.exe", String.Format("-z {0} -c \"{1}\"", dumpFilePath, loadCommand));
-            }
-            else
-            {
-                Console.WriteLine("Debug support files are now available in " + debugSupportFilesLocation);
-                Console.WriteLine("Use .load <location>\\sos to load SOS and .cordll -se -lp <location> to set up DAC");
-            }
+				if (target.Architecture == Architecture.X86 && IntPtr.Size != 4)
+				{
+					Console.WriteLine("Please use the 32 bit version of sosloader to analyze this dump.");
+					return;
+				}
+				if (target.Architecture == Architecture.Amd64 && IntPtr.Size != 8)
+				{
+					Console.WriteLine("Please use the 64 bit version of sosloader to analyze this dump.");
+					return;
+				}
+
+				string dacLocation = target.ClrVersions[0].LocalMatchingDac;
+				if (!String.IsNullOrEmpty(dacLocation))
+				{
+					Console.WriteLine();
+					Console.WriteLine($"dacLocation: {dacLocation}");
+					Console.WriteLine("The debug support files are available on the local machine.");
+					if (args[0] == "launch")
+					{
+						Console.WriteLine("Launching windbg.exe with the provided dump file...");
+						var loadByCommand = ".loadby sos " + (target.ClrVersions[0].Version.Major == 4 ? "clr" : "mscorwks");
+
+						StartWinDbg($"-z {dumpFilePath} -c \"{loadByCommand}\"");
+					}
+					return;
+				}
+
+				string debugSupportFilesLocation = DacLocator.GetDebugSupportFiles(target.ClrVersions[0], target);
+				if (args[0] == "launch")
+				{
+					Console.WriteLine("Launching windbg.exe with the provided dump file (must be in path)...");
+					var loadCommand = String.Format(".load {0}; .cordll -se -lp {1}",
+						Path.Combine(debugSupportFilesLocation, "sos"), debugSupportFilesLocation);
+
+					StartWinDbg($"-z {dumpFilePath} -c \"{loadCommand}\"");
+				}
+				else
+				{
+					Console.WriteLine("Debug support files are now available in " + debugSupportFilesLocation);
+					Console.WriteLine("Use .load <location>\\sos to load SOS and .cordll -se -lp <location> to set up DAC");
+				}
+			}
+	        finally
+	        {
+				Console.WriteLine("press any key to pause");
+		        Thread.Sleep(5000);
+		        if (Console.KeyAvailable)
+		        {
+			        Console.WriteLine("|| paused ||");
+					Thread.Sleep(1000000);
+		        }
+
+	        }
         }
+
+	    private static void StartWinDbg(string arguments)
+	    {
+		    var winDbg = @"C:\Program Files\Debugging Tools for Windows (x64)\windbg.exe";
+		    Console.WriteLine();
+		    Console.WriteLine($"start>: {winDbg} {arguments}");
+		    Process.Start(winDbg, arguments);
+	    }
     }
 }
